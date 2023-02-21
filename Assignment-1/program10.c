909f9a6314267c21cb1bee961c3e858ff4f613b2
Siddharth kumar <96335920+siddharth95kumar@users.noreply.github.com>
2023-02-21 12:40:53 +0530

    Add files via upload

---
+++ Assignment-1/program10.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int day,month,year;
+    printf("DD/MM/YYYY\n");
+    scanf("%d/%d/%d",&day,&month,&year);
+    printf("Day - %d, Month- %d, Year - %d",day,month,year);
+}
---
+++ Assignment-1/program2.c
@@ -0,0 +1,7 @@
+//Write a program to Print Hello in first line and Students in second line.
+
+#include<stdio.h>
+void main()
+{
+    printf("Hello \nStudents");
+}
